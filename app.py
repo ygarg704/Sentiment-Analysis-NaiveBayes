395c34ed25be254d719eeee9dc7cc88b3c755616
ygarg704 <ygarg704@gmail.com>
2020-05-19 16:51:27 +0530

    init commit

---
+++ app.py
@@ -0,0 +1,101 @@
+import re
+from flask import *
+
+from source.sensitive_data import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("first_page.html")
+
+
+@app.route('/second', methods=['POST'])
+def second():
+    # To calculate the basic probability of a word for a category
+    def calc_prob(word, category):
+        if word not in feature_set or word not in dataset[category]:
+            return 0
+
+        return float(dataset[category][word]) / no_of_items[category]
+
+    # Weighted probability of a word for a category
+    def weighted_prob(word, category):
+        # basic probability of a word - calculated by calc_prob
+        basic_prob = calc_prob(word, category)
+
+        # total_no_of_appearances - in all the categories
+        if word in feature_set:
+            tot = sum(feature_set[word].values())
+        else:
+            tot = 0
+
+        # Weighted probability is given by the formula
+        # (weight*assumedprobability + total_no_of_appearances*basic_probability)/(total_no_of_appearances+weight)
+        # weight by default is taken as 1.0
+        # assumed probability is 0.5 here
+        weight_prob = ((1.0 * 0.5) + (tot * basic_prob)) / (1.0 + tot)
+        return weight_prob
+
+    # To get probability of the test data for the given category
+    def test_prob(test, category):
+        # Split the test data
+        split_data = re.split('[^a-zA-Z][\'][ ]', test)
+
+        data = []
+        for i in split_data:
+            if ' ' in i:
+                i = i.split(' ')
+                for j in i:
+                    if j not in data:
+                        data.append(j.lower())
+            elif len(i) > 2 and i not in data:
+                data.append(i.lower())
+
+        p = 1
+        for i in data:
+            p *= weighted_prob(i, category)
+        return p
+
+    # Naive Bayes implementation
+    def naive_bayes(test):
+        """
+            p(A|B) = p(B|A) * p(A) / p(B)
+
+            Assume A - Category
+                B - Test data
+                p(A|B) - Category given the Test data
+
+            Here ignoring p(B) in the denominator (Since it remains same for every category)
+        """
+        results = {}
+        for i in dataset.keys():
+            # Category Probability
+            # Number of items in category/total number of items
+            cat_prob = float(no_of_items[i]) / sum(no_of_items.values())
+
+            # p(test data | category)
+            test_prob1 = test_prob(test, i)
+
+            results[i] = test_prob1 * cat_prob
+
+        return results
+
+    text = request.form['text']
+    result = naive_bayes(text)
+
+    answer = ''
+
+    if result['1'] > result['-1']:
+        answer = 'positive'
+        print(answer)
+    else:
+        answer = 'negative'
+        print(answer)
+
+    return render_template("second_page.html", output_final=answer, input_final =text)
+
+
+if __name__ == '__main__':
+    app.run()
